6890ce9cd0b2d9162fd2c5c12afb1e981dff888a
toshikazu-kawabata <toshikazu-kawabata@unitika.co.jp>
2025-03-11 10:40:57 +0900

    テスト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Recipe AI Generator",
-  description: "Meet Your Personal Recipe AI",
+  title: "樹脂事業部生成AI",
+  description: "営業情報活用",
 };
 
 export default function RootLayout({
