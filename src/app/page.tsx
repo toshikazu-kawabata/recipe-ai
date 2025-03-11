b862ca0ba07986382d75aae099adbc8e8c949efd
toshikazu-kawabata <toshikazu-kawabata@unitika.co.jp>
2025-03-11 09:42:35 +0900

    テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24  m-auto ">
       <div className=" pb-10 mx-auto text-center flex flex-col items-start -center max-w-3xl">
         <h1 className=" text-4xl  font-bold  text-gray-900 sm:text-6xl ">
-          Meet Your Personal <span className=" text-blue-600"> Recipe AI </span>
+          樹脂事業部 <span className=" text-blue-600"> 営業情報生成AI活用 </span>
           <p className=" mt-10 font-medium   text-lg  max-w-prose text-gray-900 ">
-            Simply type few ingredients using the format ingredient1,
-            ingredient12,..etc and Recipe AI will generate an all-new recipe on
-            demand...
+            SAIL、PDCA表、COMPASS売上データが参照可能です
           </p>
         </h1>
       </div>
